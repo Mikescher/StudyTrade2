2ef5bf809039b0ec94b663c4b4dac268d2e6094b
Timo Höting <timo@hoeting.eu>
2014-04-06 22:20:14 +0200

    New Landing & custom css

--- src/com/studytrade/studytrade2/Studytrade2UI.java
+++ src/com/studytrade/studytrade2/Studytrade2UI.java
@@ -2,6 +2,7 @@ package com.studytrade.studytrade2;
 
 import javax.servlet.annotation.WebServlet;
 
+import com.studytrade.studytrade2.pages.LandingPage;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
@@ -27,10 +28,23 @@ public class Studytrade2UI extends UI {
 		view = new StudyTradeView();
 		model = new StudyTradeModel();
 		presenter = new StudyTradePresenter(model, view);
+		LandingPage landing = new LandingPage();
 		
+		/* http://localhost:8080/StudyTrade2/dev
+		 * hier können verschiedene Einsprungpunkte
+		 * erstellt werden !!!
+		 */
+		
+		String pathInfo = request.getPathInfo();
+		if("/dev".equals(pathInfo)){
+			/*Landing page von timo*/
+			setContent(landing);
+		}else{
+			/*Startpage von Armin*/
 		setContent(view);
-		
 		presenter.Init();
+		}
+		
 	}
 
 }
