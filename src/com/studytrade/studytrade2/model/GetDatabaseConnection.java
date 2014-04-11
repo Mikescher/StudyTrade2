426c6332291210c424951b75859a87cd82c0d188
Timo Höting <timo@hoeting.eu>
2014-04-11 10:12:05 +0200

    Login / Register / Class for DB Connection

---
+++ src/com/studytrade/studytrade2/model/GetDatabaseConnection.java
@@ -0,0 +1,34 @@
+package com.studytrade.studytrade2.model;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class GetDatabaseConnection {
+
+	public java.sql.Connection getConnection() {
+		String url="jdbc:mysql://localhost/project_one";
+		String password="passwort";
+		String user="server";
+		java.sql.Connection con = null;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			con = DriverManager.getConnection(url, user, password);
+			
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return con;
+	}
+}
