e9982f825baf48200a82b12892683b69e8b271fe
Mike Schwörer <mailport@mikescher.de>
2014-04-14 21:22:52 +0200

    The Layout is now completely f*** up

--- src/com/studytrade/studytrade2/view/CustomStudyTradeComponent.java
+++
@@ -1,8 +0,0 @@
-package com.studytrade.studytrade2.view;
-
-import com.vaadin.ui.CustomComponent;
-
-public abstract class CustomStudyTradeComponent extends CustomComponent {
-	private static final long serialVersionUID = -8192711605278107723L;
-
-}
