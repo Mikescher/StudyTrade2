c02d12aae5dd58a4a18046ca8e045318e5f6bc50
Mike Schwörer <mailport@mikescher.de>
2014-04-15 16:22:18 +0200

    Added standard SQL Data

--- src/com/studytrade/studytrade2/model/database/MyDatabaseConnection.java
+++ src/com/studytrade/studytrade2/model/database/MyDatabaseConnection.java
@@ -5,11 +5,6 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
-
-import com.mysql.jdbc.PreparedStatement;
-import com.studytrade.studytrade2.model.StudyTradeArticle;
 
 import logging.STLog;
 
