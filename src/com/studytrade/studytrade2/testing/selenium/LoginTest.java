73653e09fbfdeb4de6f5c15ac62e4e62e9d12baf
Timo Höting <timo@hoeting.eu>
2014-05-06 22:40:55 +0200

    added more Tests for Login

--- src/com/studytrade/studytrade2/testing/selenium/LoginTest.java
+++ src/com/studytrade/studytrade2/testing/selenium/LoginTest.java
@@ -9,32 +9,76 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import com.thoughtworks.selenium.webdriven.WebDriverBackedSelenium;
 
 public class LoginTest {
-	
+
 	@Before
-    public void setUp() throws Exception {
-        ///NOP
-    }
-    
+	public void setUp() throws Exception {
+		// /NOP
+	}
+
 	@Test
-    public void testTempscript() throws Exception {
+	public void testTempscriptMike() throws Exception {
 		WebDriver driver = new FirefoxDriver();
 
 		WebDriverBackedSelenium selenium = new WebDriverBackedSelenium(driver, "http://localhost:8080/");
 
 		selenium.open("StudyTrade2/");
 
-		selenium.setTimeout("1000");
-		
-		selenium.waitForPageToLoad("1000");
-		
+		selenium.setTimeout("10000");
+
+		selenium.waitForPageToLoad("10000");
+
 		selenium.type("selendebug_CmnPg_ed_username", "Mikescher");
 		selenium.type("selendebug_CmnPg_ed_passw", "test");
-		
+
 		selenium.click("selendebug_CmnPg_btn_login");
 
-		selenium.waitForPageToLoad("1000");
-		
+		selenium.waitForPageToLoad("10000");
+
 		Assert.assertTrue(selenium.isTextPresent("Mikescher"));
 		Assert.assertTrue(selenium.isTextPresent("Log Off"));
-    }
+	}
+	@Test
+	public void testTempscripTimot() throws Exception {
+		WebDriver driver = new FirefoxDriver();
+
+		WebDriverBackedSelenium selenium = new WebDriverBackedSelenium(driver, "http://localhost:8080/");
+
+		selenium.open("StudyTrade2/");
+
+		selenium.setTimeout("10000");
+
+		selenium.waitForPageToLoad("10000");
+
+		selenium.type("selendebug_CmnPg_ed_username", "MBiel");
+		selenium.type("selendebug_CmnPg_ed_passw", "foo");
+
+		selenium.click("selendebug_CmnPg_btn_login");
+
+		selenium.waitForPageToLoad("10000");
+
+		Assert.assertTrue(selenium.isTextPresent("MBiel"));
+		Assert.assertTrue(selenium.isTextPresent("Log Off"));
+	}
+	@Test
+	public void testTempscriptMUSTFAIL() throws Exception {
+		WebDriver driver = new FirefoxDriver();
+
+		WebDriverBackedSelenium selenium = new WebDriverBackedSelenium(driver, "http://localhost:8080/");
+
+		selenium.open("StudyTrade2/");
+
+		selenium.setTimeout("10000");
+
+		selenium.waitForPageToLoad("10000");
+
+		selenium.type("selendebug_CmnPg_ed_username", "User");
+		selenium.type("selendebug_CmnPg_ed_passw", "test");
+
+		selenium.click("selendebug_CmnPg_btn_login");
+
+		selenium.waitForPageToLoad("10000");
+
+		Assert.assertTrue(selenium.isTextPresent("User"));
+		Assert.assertTrue(selenium.isTextPresent("test"));
+	}
 }
